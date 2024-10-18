212ac028ad4929cbbc8a9b25f6464ece3bf7c1bb
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2024-10-18 17:54:34 -0500

    Add Education and ContactForm

---
+++ Porfolio.WebSite/Models/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Porfolio.WebSite.Models;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no debe superar los 100 caracteres.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "El asunto es obligatorio.")]
+    [StringLength(150, ErrorMessage = "El asunto no debe superar los 150 caracteres.")]
+    public string? Subject { get; set; }
+
+    [Required(ErrorMessage = "El mensaje es obligatorio.")]
+    [StringLength(1000, ErrorMessage = "El mensaje no debe superar los 1000 caracteres.")]
+    public string? Message { get; set; }
+
+    public bool SendCopyToSelf { get; set; }
+}
--- Porfolio.WebSite/Program.cs
+++ Porfolio.WebSite/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
-using Porfolio.WebSite.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddMudServices();
 
